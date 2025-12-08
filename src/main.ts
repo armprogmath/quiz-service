b68732b3c3ac49975059acd4983a3fbd43955162
armprogmath <armprogmath@gmail.com>
2025-12-09 00:04:15 +0400

    lock.json

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import chalk from 'chalk';
+//import chalk from 'chalk';
+const chalk = require('chalk');
 
 async function bootstrap() {
   dotenv.config();
@@ -33,4 +34,5 @@ async function bootstrap() {
    await app.listen(configService.get('PORT', 3003), async () => Logger.log(`${await app.getUrl()}, API Doc: ${configService.get('APP_URL')}`))
 
 }
-bootstrap().then(() => Logger.log(chalk.italic.black.bgWhiteBright` Quiz application ` + ` successfully started in ${process.env.NODE_ENV || 'development' || 'production'} mode`));
+//bootstrap().then(() => Logger.log(chalk.italic.black.bgWhiteBright` Quiz application ` + ` successfully started in ${process.env.NODE_ENV || 'development' || 'production'} mode`));
+bootstrap().then(() => Logger.log(`Quiz application successfully started in ${process.env.NODE_ENV || 'development' || 'production'} mode`));
