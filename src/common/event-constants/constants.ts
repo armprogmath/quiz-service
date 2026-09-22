16899d1f35b01d31a710fbe2b269b14959283751
armprogmath <armprogmath@gmail.com>
2026-09-22 22:04:29 +0400

    changed unused constants

--- src/common/event-constants/constants.ts
+++ src/common/event-constants/constants.ts
@@ -25,8 +25,8 @@ export enum Consumers {
 
 export enum ExchangeType {
     Topic = "topic",
-    Fanout = "fanout",
-    Direct = "direct"
+    //Fanout = "fanout",
+    //Direct = "direct"
 }
 
 export enum Topics {
